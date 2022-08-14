dfa86710b9ee742e48ce7596e25fdbf01ee91d9f
neokaiyuan <kai@rocketacademy.co>
2022-08-14 11:19:13 +0800

    Add Rocket's Google Ads ID to GTag tracking for conversions

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,8 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "UA-160206985-1", // Google Analytics / GA
-          "AW-10817488949", // Google Ads / Adwords / AW
+          "AW-610708425", // Google Ads / Adwords / AW (Rocket Academy account)
+          "AW-10817488949", // Google Ads / Adwords / AW (Teresa's unnamed account)
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
