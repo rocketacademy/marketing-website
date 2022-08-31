0c09be6fd88f707f5c32e854782c166d9a6da21a
neokaiyuan <kai@rocketacademy.co>
2022-08-31 10:43:33 +0800

    Hardcode Bootcamp course dates to prevent calendar issues when no dates in course GCal

--- src/components/StartCodingSection.js
+++ src/components/StartCodingSection.js
@@ -1,50 +1,7 @@
 import React from "react";
 import GetStartedButton from "./GetStartedButton";
 
-const StartCodingSection = ({ upcomingCourseDates }) => {
-  let nextBasicsStartDate;
-  let nextFulltimeStartDate;
-  let nextParttimeStartDate;
-
-  if (upcomingCourseDates) {
-    const basicsDates = upcomingCourseDates.filter((course) =>
-      course.node.summary.includes("Basics")
-    );
-    nextBasicsStartDate = basicsDates[0].node.start.dateTime;
-
-    const fulltimeDates = upcomingCourseDates.filter((course) =>
-      course.node.description.includes("Full Time")
-    );
-    nextFulltimeStartDate = fulltimeDates[0].node.start.dateTime;
-
-    const parttimeDates = upcomingCourseDates.filter((course) =>
-      course.node.description.includes("Part Time")
-    );
-    nextParttimeStartDate = parttimeDates[0].node.start.dateTime;
-
-    const getFormattedDate = (d) => {
-      const dateObj = new Date(d);
-      const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(
-        dateObj
-      );
-      const mo = new Intl.DateTimeFormat("en", { month: "short" }).format(
-        dateObj
-      );
-      const da = new Intl.DateTimeFormat("en", { day: "numeric" }).format(
-        dateObj
-      );
-      return `${da} ${mo} ${ye}`;
-    };
-
-    nextBasicsStartDate = getFormattedDate(nextBasicsStartDate);
-    nextFulltimeStartDate = getFormattedDate(nextFulltimeStartDate);
-    nextParttimeStartDate = getFormattedDate(nextParttimeStartDate);
-  } else {
-    nextBasicsStartDate = "<< dynamic date from gcal >>";
-    nextFulltimeStartDate = "<< dynamic date from gcal >>";
-    nextParttimeStartDate = "<< dynamic date from gcal >>";
-  }
-
+const StartCodingSection = () => {
   return (
     <div className="container-fluid startcoding-outer-container">
       <div className="container startcoding-inner-container">
@@ -59,17 +16,13 @@ const StartCodingSection = ({ upcomingCourseDates }) => {
               <div className="startcoding-bootcampft-text">
                 Bootcamp (Full Time)
               </div>
-              <div className="startcoding-bootcampft-date">
-                {nextFulltimeStartDate}
-              </div>
+              <div className="startcoding-bootcampft-date">17 Oct 2022</div>
             </div>
             <div className="startcoding-info">
               <div className="startcoding-bootcampft-text">
                 Bootcamp (Part Time)
               </div>
-              <div className="startcoding-bootcampft-date">
-                {nextParttimeStartDate}
-              </div>
+              <div className="startcoding-bootcampft-date">26 Nov 2022</div>
             </div>
           </div>
         </div>
