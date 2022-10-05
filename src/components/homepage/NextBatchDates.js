7e128c3a5e54af97276bdb792256253a402cde7a
neokaiyuan <kai@rocketacademy.co>
2022-10-05 17:15:40 +0800

    Clean up logic

--- src/components/homepage/NextBatchDates.js
+++ src/components/homepage/NextBatchDates.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 const NextBatchDates = ({ details, upcomingCourseDates }) => {
-  let heading = "";
-
   if (details.heading === "Coding Basics") {
-    heading = "Basics";
-  } else if (details.heading === "Coding Bootcamp") {
-    heading = "Bootcamp";
+    return (
+      <div>
+        <li>
+          ✓ Upcoming course dates: 21 Nov 2022 - 16 Jan 2023, 26 Jan 2023 - 6
+          Mar 2023
+        </li>
+      </div>
+    );
   }
 
   let nextStartDate = "";
@@ -15,14 +18,10 @@ const NextBatchDates = ({ details, upcomingCourseDates }) => {
   if (details.type === "Part Time") {
     nextStartDate = new Date("2022-11-26");
     nextEndDate = new Date("2023-06-20");
-  }
-  if (details.type === "Full Time") {
+  } else if (details.type === "Full Time") {
     nextStartDate = new Date("2022-10-17");
     nextEndDate = new Date("2023-02-10");
   }
-
-  let startDate = nextStartDate;
-  let endDate = nextEndDate;
 
   const getFormattedDate = (d) => {
     const dateObj = new Date(d);
@@ -38,20 +37,10 @@ const NextBatchDates = ({ details, upcomingCourseDates }) => {
     return `${da} ${mo} ${ye}`;
   };
 
-  const displayDate = `${getFormattedDate(startDate)} - ${getFormattedDate(
-    endDate
+  const displayDate = `${getFormattedDate(nextStartDate)} - ${getFormattedDate(
+    nextEndDate
   )}`;
 
-  if (heading === "Basics") {
-    return (
-      <div>
-        <li>
-          ✓ Upcoming course dates: 21 Nov 2022 - 16 Jan 2023, 26 Jan 2023 - 6
-          Mar 2023
-        </li>
-      </div>
-    );
-  }
   return <li>✓ Next batch: {displayDate}</li>;
 };
 
